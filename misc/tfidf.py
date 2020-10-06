53cd594fc2e942ae7055405e4abf1fc9c1887e9b
idea-misw <1iota4delta3epsilon3alpha9@gmail.com>
2020-10-06 21:22:22 +0900

    Update TF-IDF

--- misc/tfidf.py
+++ misc/tfidf.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'generator'))
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-from tokenizer import Tokenizer
+# from tokenizer import Tokenizer
 from generator import Generator
 
 dump_path = os.path.join(
@@ -15,15 +15,17 @@ dump_path = os.path.join(
     'temp_data.pickle'
 )
 
-t = Tokenizer()
+# t = Tokenizer()
 
 g = Generator()
 g.load(dump_path)
 
 corpus = g.generates(20)
-corpus.append('外寒すぎてデッケェ冷えピタに包まれたときと全く同じ気持ちになった')
+# corpus.append('外寒すぎてデッケェ冷えピタに包まれたときと全く同じ気持ちになった')
+corpus.append('ざむちゃん実は戦隊モノの黄色を担当しているんだよね')
 
-vectorizer = TfidfVectorizer(tokenizer=t.tokenize)
+# vectorizer = TfidfVectorizer(tokenizer=t.tokenize)
+vectorizer = TfidfVectorizer(analyzer='char')
 X = vectorizer.fit_transform(corpus)
 
 print(corpus[cosine_similarity(X)[-1, :-1].argmax()])
